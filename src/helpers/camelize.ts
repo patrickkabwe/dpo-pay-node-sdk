c256e9a2f3d8cfe9708f0f0c0edd79f2020aac3c
Patrick Kabwe <patrickckabwe@gmail.com>
2024-02-16 09:17:18 +0200

    refactor: camelize func

--- src/helpers/camelize.ts
+++ src/helpers/camelize.ts
@@ -1,5 +1,8 @@
 export function camelize(str: string) {
   return str
+    .replace(/[\s_-]+(\w)/g, function (match, firstLetter) {
+      return firstLetter.toUpperCase();
+    }) 
     .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
       return index === 0 ? word.toLowerCase() : word.toUpperCase();
     })
